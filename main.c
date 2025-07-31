f553385a4fed50eed598466e2a3dc1db5b1fe3df
CCSStudent111 <160704838+CCSStudent111@users.noreply.github.com>
2025-07-31 21:50:18 +0800

    added time

--- main.c
+++ main.c
@@ -7,9 +7,13 @@ extern int accelerationcalculator(float vi, float vf, float time);
 int main() {
 	int rows;
 	int i, accel, nums;
-	float vi, vf, time;
+	float vi, vf, time, cputime;
 	char line[100];
 	int accelerations[100];
+	LARGE_INTEGER frequency, start, end;
+	double ns;
+
+	QueryPerformanceFrequency(&frequency);
 
 	// get inputs
 	scanf_s(" %d", &rows);
@@ -17,27 +21,33 @@ int main() {
 
 	for (i = 0; i < rows; i++) {
 
-		//printf("Row %d: ", i + 1);
+		printf("Row %d: ", i + 1);
 		nums = 0;
 		
 		fgets(line, sizeof(line), stdin);
 		nums = sscanf_s(line, "%f, %f, %f\n", &vi, &vf, &time);
 
+		QueryPerformanceCounter(&start);
 		if (nums == 3) {
-			accelerations[i] = accelerationcalculator(vi, vf, time);
-		
+			
+			for (int j = 0; j < 30; j++){
+				accelerations[i] = accelerationcalculator(vi, vf, time);
+			}
+			
 		}
 		else {
 			nums = 0;  // if fgets failed
 			printf("Error reading line %d.", i + 1);
 		}
-		
+		QueryPerformanceCounter(&end);
 	}
 
 	//print output
 	for (i = 0; i < rows; i++) {
 		printf("\n%d\n", accelerations[i]);
 	}
+	ns = (double)(end.QuadPart - start.QuadPart) * 1e9 / frequency.QuadPart;
+	printf("\nTime: %.0f ns", ns);
 
 	return 0;
 }
